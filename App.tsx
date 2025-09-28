92fb8f8d8f465e1301a7fcd3e6b60fe98c2ac591
eugeniozucal <eugenio.zucal@gmail.com>
2025-09-28 10:57:41 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,1 @@
 
-import React from 'react';
-
-const App: React.FC = () => {
-  return (
-    <main className="bg-gray-50 dark:bg-gray-900 min-h-screen flex items-center justify-center font-sans">
-      <div className="text-center">
-        <h1 className="text-6xl md:text-8xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-teal-400 dark:from-blue-400 dark:to-teal-300 animate-pulse">
-          hello
-        </h1>
-      </div>
-    </main>
-  );
-};
-
-export default App;
